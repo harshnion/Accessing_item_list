9ec678864b6955b13740c768f2e4e5058c06c9ac
harshnion <86431354+harshnion@users.noreply.github.com>
2021-07-01 22:14:17 +0530

    8_commit

---
+++ Access_list.py
@@ -0,0 +1,12 @@
+# Hello guys How's your coding going on
+
+# Today we will learn about how to access a single item from a list
+m = ["egg","banana","milk"]
+print(m)
+
+# now we change 2 element of list to butter
+m[1] = "butter"
+print(m)
+
+# it's just a try how we will access a list item from last
+print(m[-1])
